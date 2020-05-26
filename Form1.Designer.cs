becfdfa1c8f68b435c4619121562073dfeb24d6d
Abah Joseph Israel <42052027+abahjosephisrael@users.noreply.github.com>
2020-05-26 12:41:35 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Analog_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblDigital = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(492, 368);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblDigital
+            // 
+            this.lblDigital.AutoSize = true;
+            this.lblDigital.BackColor = System.Drawing.Color.White;
+            this.lblDigital.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDigital.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblDigital.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDigital.Location = new System.Drawing.Point(166, 242);
+            this.lblDigital.Name = "lblDigital";
+            this.lblDigital.Size = new System.Drawing.Size(2, 24);
+            this.lblDigital.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SIR JOH";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(492, 368);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDigital);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Analog Clock";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblDigital;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analog_Clock
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+        int WIDTH = 300, HEIGHT = 300, secHAND = 140, minHAND = 110, hrHAND = 80;
+
+        //center
+        int cx, cy;
+        string meri;
+        Bitmap bmp;
+        Graphics g;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(WIDTH + 1, HEIGHT + 1);
+
+            // Center
+            cx = WIDTH / 2;
+            cy = HEIGHT / 2;
+
+            //Background color
+            this.BackColor = Color.White;
+
+            //Timer
+            t.Interval = 1000;
+            t.Tick += new EventHandler(this.t_Tick);
+            t.Start();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            // create a graphics
+            g = Graphics.FromImage(bmp);
+            // get time
+            int ss = DateTime.Now.Second;
+            int mm = DateTime.Now.Minute;
+            int hh = DateTime.Now.Hour;
+            
+            if (hh > 12)
+            {
+                hh = hh - 12;
+                meri = "PM";
+            }
+            else if(hh==0)
+                {
+                hh = 12;
+                meri = "AM";
+                }
+            else
+            {
+                meri = "AM";
+            }
+            lblDigital.Text = "[ " + hh + ":" + mm + ":" + ss + "  " + meri + " ]";
+
+            int[] handCoord = new int[2];
+
+            //clear
+            g.Clear(Color.Transparent);
+
+            g.DrawEllipse (new Pen(Color.Black, 1f), 0, 0, WIDTH, HEIGHT);
+
+            // draw numbers
+            g.DrawString("12", new Font("Arial", 12), Brushes.Black, new Point(140, 2));
+            g.DrawString("11", new Font("Arial", 12), Brushes.Black, new Point(75, 25));
+            g.DrawString("10", new Font("Arial", 12), Brushes.Black, new Point(25, 75));
+            g.DrawString("9", new Font("Arial", 12), Brushes.Black, new Point(0, 140));
+            g.DrawString("8", new Font("Arial", 12), Brushes.Black, new Point(15, 210));
+            g.DrawString("7", new Font("Arial", 12), Brushes.Black, new Point(75, 262));
+            g.DrawString("6", new Font("Arial", 12), Brushes.Black, new Point(142, 282));
+            g.DrawString("5", new Font("Arial", 12), Brushes.Black, new Point(215, 262));
+            g.DrawString("4", new Font("Arial", 12), Brushes.Black, new Point(270,208));
+            g.DrawString("3", new Font("Arial", 12), Brushes.Black, new Point(286, 140));
+            g.DrawString("2", new Font("Arial", 12), Brushes.Black, new Point(262, 75));
+            g.DrawString("1", new Font("Arial", 12), Brushes.Black, new Point(210, 25));
+
+            //second hand
+            handCoord = msCoord(ss, secHAND);
+            g.DrawLine(new Pen(Color.Red, 1f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //minute hand
+            handCoord = msCoord(mm, minHAND);
+            g.DrawLine(new Pen(Color.Black, 2f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //hour hand
+            handCoord = hrCoord(hh%12, mm, hrHAND);
+            g.DrawLine(new Pen(Color.Gray, 3f), new Point(cx, cy), new Point(handCoord[0], handCoord[1]));
+
+            //load picture into picture box
+            pictureBox1.Image = bmp;
+
+            //display time
+            if (hh > 12)
+            {
+                hh = hh - 12;
+                //this.Text = "Analog Clock [ " + hh + ":" + mm + ":" + ss + " "+ meri + " ]";
+            }
+            else
+            {
+                //this.Text = "Analog Clock [ " + hh + ":" + mm + ":" + ss + " " + meri + " ]";
+            }
+           // this.Text = "Analog Clock [ " + hh + ":" + mm + ":" + ss + meri + "]";
+
+            //dispose
+            g.Dispose();
+
+        }
+
+        //Coordinate for minute
+        private int [] msCoord(int val, int hlen)
+        {
+            int [] coord = new int[2];
+            val *= 6; //
+            if(val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+
+        //coord for hour hand
+
+        private int[] hrCoord(int hval, int mval, int hlen)
+        {
+            int[] coord = new int[2];
+
+
+            // each second makes 30 degrees
+            // each minute makes 0.5 degree
+            int val = (int)((hval * 30) + (mval * 0.5));
+
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+
+    }
+}
